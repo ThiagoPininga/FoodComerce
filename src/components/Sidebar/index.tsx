e80b4bf295f43a7f090abef755d5648496bf2ceb
Thiago <thiagopininga@hotmail.com>
2023-05-28 18:11:43 -0300

    primeiro commit

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,28 +1,54 @@
-import { Container } from "./styles"
+import { useState } from 'react'
+
+import { Container } from './styles'
 
 import { ReactComponent as BurgerIcon } from '../../assets/burger.svg'
+import { ReactComponent as PizzaIcon } from '../../assets/pizza.svg'
+import { ReactComponent as SodaPopIcon } from '../../assets/soda.svg'
+import { ReactComponent as IceCreamIcon } from '../../assets/ice-cream.svg'
 
 import menuImg from '../../assets/menu.svg'
 
-
 export function Sidebar() {
-  return <Container>
-    <button type="button">
-      <img src={menuImg} alt="Botão de Menu"></img>
-    </button>
+  const [menuOpen, setMenuOpen] = useState(false)
 
-    <nav>
-      <ul>
-        <li>
-          <a href="#" className="active">
-            <BurgerIcon></BurgerIcon>
-            <span>Hambúrgueres</span>
-          </a>
-        </li>
-        <li></li>
-        <li></li>
-        <li></li>
-      </ul>
-    </nav>
-  </Container>
+  const handleToggleMenu = () => {
+    setMenuOpen(!menuOpen)
+  }
+
+  return (
+    <Container isMenuOpen={menuOpen}>
+      <button type='button' onClick={handleToggleMenu}>
+        <img src={menuImg} alt='Abrir e fechar o menu' />
+      </button>
+      <nav>
+        <ul>
+          <li>
+            <a href='#' className='active'>
+              <BurgerIcon />
+              <span>Hambúrgueres</span>
+            </a>
+          </li>
+          <li>
+            <a href='#'>
+              <PizzaIcon />
+              <span>Pizzas</span>
+            </a>
+          </li>
+          <li>
+            <a href='#'>
+              <SodaPopIcon />
+              <span>Bebidas</span>
+            </a>
+          </li>
+          <li>
+            <a href='#'>
+              <IceCreamIcon />
+              <span>Sorvetes</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </Container>
+  )
 }
