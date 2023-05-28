dd30021edc7b762627d692660a29209c76fa68fd
thiago <thiagopininga@hotmail.com>
2023-05-28 14:38:42 -0300

    indo pra a casa de kai

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,3 +1,28 @@
+import { Container } from "./styles"
+
+import { ReactComponent as BurgerIcon } from '../../assets/burger.svg'
+
+import menuImg from '../../assets/menu.svg'
+
+
 export function Sidebar() {
-  return <h1>Sidebar</h1>
+  return <Container>
+    <button type="button">
+      <img src={menuImg} alt="Botão de Menu"></img>
+    </button>
+
+    <nav>
+      <ul>
+        <li>
+          <a href="#" className="active">
+            <BurgerIcon></BurgerIcon>
+            <span>Hambúrgueres</span>
+          </a>
+        </li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </nav>
+  </Container>
 }
