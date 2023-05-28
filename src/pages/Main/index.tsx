aa514e66d31c857e77dd645d9f8c356bf7a60bd7
thiago <thiagopininga@hotmail.com>
2023-05-28 13:14:54 -0300

    upando para o github

---
+++ src/pages/Main/index.tsx
@@ -0,0 +1,11 @@
+import { Sidebar } from '../../components/Sidebar'
+
+import { Container } from './styles'
+
+export default function Main() {
+  return (
+    <Container>
+      <Sidebar/>
+    </Container>
+  )
+}
