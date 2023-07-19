669673bf9da6d643be580bf639eedb13b13b4cd5
thiago <thiagopininga@hotmail.com>
2023-07-19 13:06:12 -0300

    formatação da pagina Carrinho

--- src/pages/MyCart/Table/tableDesktop/index.tsx
+++ src/pages/MyCart/Table/tableDesktop/index.tsx
@@ -2,6 +2,10 @@ import { currencyFormat } from "../../../../Helpers/currencyFormat";
 
 import { useCart } from "../../../../hooks/useCart"
 
+import PlusImg  from "../../../../assets/circle-plus.svg"
+import MinusImg from "../../../../assets/circle-minus.svg"
+
+
 import { Container } from "./styles"
 
 export function TableDesktop(){
@@ -26,16 +30,30 @@ export function TableDesktop(){
                 <td>
                   <img src={item.image} alt={item.name}/>
                 </td>
+
                 <td>
                   <h4>{item.name}</h4>
                   <span>{currencyFormat(item.price)}</span>
                 </td>
+
                 <td>
-                  {item.quantity}
+                  <div>
+                    <button type="button" onClick={() => console.log(`Decrementar snack`, item)}>
+                      <img src={MinusImg} alt="Remover qtd" />
+                    </button>
+                    <span>
+                      {`${item.quantity}`.padStart(2, '0')}
+                    </span>
+                    <button type="button" onClick={() => console.log(`Aumentar snack`, item)}>
+                      <img src={PlusImg} alt="Adicionar qtd" />
+                    </button>
+                  </div>
                 </td>
+
                 <td>
                   <h5>{currencyFormat(item.subtotal)}</h5>
                 </td>
+
                 <td>
                   Deletar
                 </td>
